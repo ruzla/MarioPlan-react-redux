16da7be55ca58a70b474f1898d79dafac1fd9840
Russell <russelldoe@hotmail.co.uk>
2019-08-26 17:56:38 +0100

    dashboard component setup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import NavBar from './components/layout/NavBar';
+import Dashboard from './components/dashboard/Dashboard';
 
 class App extends Component {
   render(){
@@ -9,6 +10,9 @@ class App extends Component {
     <BrowserRouter>
       <div className="App">
         <NavBar />
+        <Switch>
+          <Route path='/' component={Dashboard} />
+        </Switch>
       </div>
     </BrowserRouter>
   );
